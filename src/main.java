0c8f8fabe81bc9c124d25e05c6c8e5c4ef6c92c7
Mat <matry7@gmail.com>
2018-11-12 16:52:36 +0100

    Added test program in String format. Prints out in Console

--- src/main.java
+++ src/main.java
@@ -1,9 +1,30 @@
 
 public class main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("test");
-	}
+        System.out.println("Test Program");
+
+        System.out.println("" +
+                "x := 0;\n" +
+                "A[0] := -1;\n" +
+                "A[1] := 0;\n" +
+                "A[2] := 1;\n" +
+                "R := (0, 0);\n" +
+                "R.fst := 1;\n" +
+                "R.snd := 2;\n" +
+                "b1 = true;\n" +
+                "b2 = false;\n" +
+                "\n" +
+                "if [b1] then \n" +
+                "\twhile [b2] do\n" +
+                "\t\tx := A[0];\n" +
+                "else\n" +
+                "\twhile [!b2] do\n" +
+                "\t\tx := A[2];\n" +
+                "Read(int x)\n" +
+                "Write(x)\n"
+        );
+    }
 
 }
