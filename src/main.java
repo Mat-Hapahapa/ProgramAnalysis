5cc7dbfa2b86956c7507032c252c5ca8def161e2
Mat <matry7@gmail.com>
2018-11-19 15:33:02 +0100

    started on worklist. made test program

--- src/main.java
+++ src/main.java
@@ -1,3 +1,8 @@
+import models.Node;
+import models.WorklistLIFO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
 
@@ -5,17 +10,36 @@ public class main {
 
         System.out.println("Test Program");
 
+
+
+
+
+
+    }
+
+    public ArrayList<String> getSimpleProgram(){
+        ArrayList<String> program = new ArrayList<String>();
+        program.add("0,1,y:=1");
+        program.add("1,2,x>0");
+        program.add("2,3,y:=x*y");
+        program.add("3,1,x:=x-1");
+        program.add("1,4,!x>0");
+        return program;
+    }
+
+    public ArrayList<String> getAdvancedProgram(){
+
         System.out.println("" +
-                "x := 0;\n" +
-                "A[0] := -1;\n" +
-                "A[1] := 0;\n" +
-                "A[2] := 1;\n" +
-                "R := (0, 0);\n" +
-                "R.fst := 1;\n" +
-                "R.snd := 2;\n" +
-                "b1 = true;\n" +
-                "b2 = false;\n" +
-                "\n" +
+                "1,2,x := 0" +
+                "2,3,A[0] := -1" +
+                "3,4,A[1] := 0" +
+                "4,5,A[2] := 1" +
+                "5,6,R := (0, 0)" +
+                "6,7,R.fst := 1" +
+                "7,8,R.snd := 2" +
+                "8,9,b1 = true" +
+                "9,10,b2 = false" +
+
                 "if [b1] then \n" +
                 "\twhile [b2] do\n" +
                 "\t\tx := A[0];\n" +
@@ -25,6 +49,46 @@ public class main {
                 "Read(int x)\n" +
                 "Write(x)\n"
         );
+
+        return new ArrayList<String>();
     }
 
+    public void worklistAlgorithm(ArrayList<Node> G) {
+        // Operations on worklist:
+        // empty = is the worklist empty
+        // Insert = returns a new worklist that is as W except it has a new constraint
+        // extract = returns a pair whose first component is a constraint x w t in the worklist and whose second component is the smaller worklist obtained by removing an occurrence of x w t; it is normally used as in
+
+
+        List<Node> W = new ArrayList<Node>(); // W := empty;
+        List<Node> infl = new ArrayList<Node>();
+
+        for(Node node : G) {  // for( x w t in S)
+            W = WorklistLIFO.insert(node, W); //  := insert((x w t),W);       all constraints in the worklist
+            //Analysis[x] := ⊥;   // the least element of L
+            //infl[node.getToNode()] //infl[x] := ∅;
+        }
+
+        /*
+
+        for (x w t in S) {
+            for (x0 in FV(t)) {
+                infl[x0] := infl[x0] ∪ {x w t}; // changes to x0 might influence x via the constraint x w t
+            }
+        }
+
+        while (!W.empty()) { //while (W != empty)
+            ((x w t),W) := extract(W); // consider the next constraint
+            new := eval(t,Analysis);
+
+            if (Analysis[x] 6w new) {        // any work to do?
+                Analysis[x] := Analysis[x] t new;  // update the analysis info.
+            }
+        }
+
+        for (x0 w t0 in infl[x]) {
+            W := insert((x0 w t0),W);  // update the worklist
+        }
+    }*/
+    }
 }
