3660a549ab8d9731205a26ca24a188d8a2dc647e
Mario Padilla Carrión <mca@expandit.com>
2018-11-19 15:26:43 +0100

    define constructor in ProgramGraph

--- src/models/ProgramGraph.java
+++ src/models/ProgramGraph.java
@@ -1,14 +1,27 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ProgramGraph {
 
-    List<Node> nodes = new ArrayList<Node>();
+    List<Node> nodes;
 
-    public ProgramGraph() {
-
+    public ProgramGraph(ArrayList<String> path) {
+    	this.nodes = new ArrayList<Node>();
+    	
+    	for(String p: path) {
+    		ArrayList<String> pList = (ArrayList<String>)Arrays.asList(p.trim().split(","));
+    		
+    		// Node (1,2,x:=0)
+    		int startNode = Integer.valueOf(pList.get(0));
+    		int endNode = Integer.valueOf(pList.get(1)); 
+    		String op = pList.get(2);
+    		
+    		nodes.add(new Node(startNode, endNode, op));
+    	}
+    	
     }
 
     public List<Node> getNodes() {
