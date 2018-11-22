3b8043195077e741e9495319033a83bafe6d92ef
Mario Padilla Carrión <mca@expandit.com>
2018-11-22 17:14:21 +0100

    Display VariableList

--- src/ReachingDefinition.java
+++ src/ReachingDefinition.java
@@ -5,18 +5,23 @@ import java.util.Map;
 import java.util.Set;
 
 import models.Node;
+import models.VariableList;
 
 public class ReachingDefinition {
 
 	public Map<Integer, ArrayList<Node>> nodeList;
 	Set<String> leastElement = new HashSet<>();
+	VariableList vl = new VariableList();
 	
 	public ReachingDefinition() {
 		nodeList = new HashMap<Integer, ArrayList<Node>>();
 	}
 
 	public void initAnalize(Node node) {
-
+		ArrayList<String> varlist = vl.getVariables(node.getOperation());
+		for(String s: varlist) {
+			leastElement.add(s);
+		}
 	}
 
 	public void analize(Node node) {
